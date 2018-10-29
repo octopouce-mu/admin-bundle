f7e175cd671dfecad52bbdf5a02149a10e684419
Kévin Hilairet <kevin@octopouce.mu>
2018-10-29 14:14:27 +0400

    update

--- Form/FileEntityType.php
+++ Form/FileEntityType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class FileType extends AbstractType
+class FileEntityType extends AbstractType
 {
 
 	public function buildForm(FormBuilderInterface $builder, array $options)

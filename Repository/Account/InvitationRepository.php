726d412899378a0d86b37767934aaa033174a2cc
Kevin Hilairet <khilairet@bhvr.com>
2020-05-01 12:36:41 -0400

    fix: deprecated Registry doctrine

--- Repository/Account/InvitationRepository.php
+++ Repository/Account/InvitationRepository.php
@@ -4,7 +4,7 @@ namespace Octopouce\AdminBundle\Repository\Account;
 
 use App\Entity\Account\Invitation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Invitation|null find( $id, $lockMode = null, $lockVersion = null )
@@ -13,7 +13,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method Invitation[]    findBy( array $criteria, array $orderBy = null, $limit = null, $offset = null )
  */
 class InvitationRepository extends ServiceEntityRepository {
-	public function __construct( RegistryInterface $registry ) {
+	public function __construct( ManagerRegistry $registry ) {
 		parent::__construct( $registry, Invitation::class );
 	}
 
